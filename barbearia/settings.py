b762c60083ab824f1e0090a37caec21ab59a49b4
AndreDAzevedo <andrecoelhodeazevedo@gmail.com>
2024-12-09 10:39:45 -0300

    Adicionado domínio ao ALLOWED_HOSTS

--- barbearia/settings.py
+++ barbearia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$9h-tlj%=m&rp%tl@u-bo3d$!+i%3*^3mj_87kqme=-uoov3=$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['barbearia-andre-a95ac35427eb.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
